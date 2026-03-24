7bcda4f010c2cc68bb7c03bc46b2f2f5ea082fae
Vladislav S <vladislavs@brightdata.com>
2026-03-24 17:00:44 +0200

    debug: add log to CreatePackage

--- windows/BrightSdkModule/ReactPackageProvider.cpp
+++ windows/BrightSdkModule/ReactPackageProvider.cpp
@@ -11,7 +11,9 @@ using namespace winrt::Microsoft::ReactNative;
 namespace winrt::BrightSdkModule::implementation {
 
 void ReactPackageProvider::CreatePackage(IReactPackageBuilder const &packageBuilder) noexcept {
+  BrightSdk::LogToFile(L"[BrightSDK] CreatePackage called\n");
   AddAttributedModules(packageBuilder, true);
+  BrightSdk::LogToFile(L"[BrightSDK] CreatePackage done\n");
 }
 
 } // namespace winrt::BrightSdkModule::implementation
